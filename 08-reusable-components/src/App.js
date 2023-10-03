303abb93b2179469860f89e5ec945befd96c11d3
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-10-03 20:38:09 +0500

    Условный рендеринг

--- 08-reusable-components/src/App.js
+++ 08-reusable-components/src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import MyComponent from "./components/MyComponent";
 
 function App() {
-    return <div className="App"></div>;
+    const obj = {
+        name: "Daniil",
+        age: 25,
+        city: "Tyumen",
+        hasPet: true,
+    };
+
+    return (
+        <div className="App">
+            <MyComponent obj={obj} />
+        </div>
+    );
 }
 
 export default App;
