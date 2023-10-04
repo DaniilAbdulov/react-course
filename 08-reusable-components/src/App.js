cec6978ee662d3a92209008d60f8a3482c0fa921
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-10-04 18:59:26 +0500

    input

--- 08-reusable-components/src/App.js
+++ 08-reusable-components/src/App.js
@@ -2,10 +2,14 @@
 import "./App.css";
 import MyComponent from "./components/MyComponent";
 // import persons from "./data/persons";
-
+import Login from "./components/Login";
 const num = 10;
 function App() {
-    return <div className="App">{num < 20 && <MyComponent />}</div>;
+    return (
+        <div className="App">
+            <Login />
+        </div>
+    );
 }
 
 export default App;
