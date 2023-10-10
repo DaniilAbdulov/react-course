109191de934e06330af4c89f8a00916467b71bb6
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-10-10 06:43:13 +0500

    dispatch action

--- 08-reusable-components/frontend/src/App.js
+++ 08-reusable-components/frontend/src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
+import BookList from "./components/BookList/BookList";
+import BookForm from "./components/BookForm/BookForm";
+import Filter from "./components/Filter/Filter";
 function App() {
     return (
         <div className="App">
             <header className="app-header">
                 <h1>App componetnt</h1>
             </header>
+            <main className="app-main">
+                <div className="app-left-column">
+                    <BookForm />
+                </div>
+                <div className="app-right-column">
+                    {/* <Filter /> */}
+                    <BookList />
+                </div>
+            </main>
         </div>
     );
 }
