654cb83356625b3f3f54ebe3c897f16ba2518573
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-10-10 06:19:25 +0500

    initial book library

---
+++ 08-reusable-components/frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+function App() {
+    return (
+        <div className="App">
+            <header className="app-header">
+                <h1>App componetnt</h1>
+            </header>
+        </div>
+    );
+}
+
+export default App;
--- 08-reusable-components/src/components/Wrapper.js
+++
@@ -1,7 +0,0 @@
-function Wrapper(props) {
-    console.log(props);
-    return (
-        <div style={{ backgroundColor: props.color }}> {props.children} </div>
-    );
-}
-export default Wrapper;
